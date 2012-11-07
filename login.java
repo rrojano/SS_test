5e847dc6788a22d711e1d5ceaa3d306740394652
Jose Maria Hernandez Garcia <chemahg@yahoo.com.mx>
2012-11-07 10:50:48 -0600

    Login
    Aqui el login

---
+++ login.java
@@ -0,0 +1,17 @@
+Public class Login{
+
+
+private String Usuario;
+private String pass;
+
+
+public Login (){}
+
+private void setUsuario(String Usuario){
+	this.usuario=Usuario;
+}
+
+private void setpass (String pass){
+	this.pass=pass;
+}
+
